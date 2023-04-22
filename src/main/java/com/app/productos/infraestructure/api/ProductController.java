27af214b4bbbc1f8d8f3a3f5e4a166b2b9dcfed2
Diego213073 <ma213073@gmail.com>
2023-04-21 22:26:18 -0500

    agregando endpoint buscar producto

--- src/main/java/com/app/productos/infraestructure/api/ProductController.java
+++ src/main/java/com/app/productos/infraestructure/api/ProductController.java
@@ -6,6 +6,7 @@ import com.app.productos.infraestructure.mappers.ProductMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,5 +28,11 @@ public class ProductController {
                 .stream().map(productMapper::modelToDto)
                 .toList();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping
+    public ResponseEntity<ProductDTO> findByid(@RequestParam Integer id) {
+        var product = productMapper.modelToDto(productServicePort.findById(id));
+        return ResponseEntity.ok(product);
     }
 }
